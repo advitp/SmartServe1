ac444d9035a115ff59530ac9eec1440382af2528
advitp <110667042+advitp@users.noreply.github.com>
2023-12-31 15:35:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import ProductDisplay from './components/ProductDisplay';
+import FileUpload from './components/FileUpload';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductDisplay/>
+    </div>
+  );
+}
+
+export default App;
